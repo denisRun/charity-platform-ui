11b31a6505efc5e34ed9e2d6e85432c7a56cf238
denisRun <polozov.denis77@gmail.com>
2023-02-27 20:44:36 +0200

    update home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,7 +5,7 @@ import { inject, observer } from 'mobx-react'
 import { StoreComponent, useStore } from '../contexts/StoreContext'
 import Header from '../components/Header'
 import Body from '../components/Body'
-import TextXs from '../components/Text/TextXs'
+import ImagePeople from '../images/home-page-people.png';
 
 const HomePage: FC = observer(() => {
   const navigate = useNavigate();
@@ -24,7 +24,20 @@ const HomePage: FC = observer(() => {
     <div>
         <Header />
         <Body>
-            <TextXs>Body text</TextXs>
+            <p className="black"
+               style={{position:"absolute", top:180, left:40, fontSize:50, fontWeight:"bold"}}>
+                We are united for you 
+            </p>
+            <p className="black"
+               style={{position:"absolute", top:280, left:40, fontSize:30, maxWidth:400}}>
+                Together making the world a better place to live 
+            </p>
+            <p className="black"
+               style={{position:"absolute", bottom:70, left:40, fontSize:20}}>
+                Over 1000 users are already here. Join us now
+            </p>   
+            <img src={ImagePeople}
+                style={{position:"absolute", bottom:0, right:20,  width: "60%", height:"75%"}} />         
         </Body>
     </div>
   )

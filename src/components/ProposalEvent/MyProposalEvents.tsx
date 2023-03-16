27bf7b76b09ae5b346cec9c1c193388b89615d2c
denisRun <polozov.denis77@gmail.com>
2023-03-16 21:05:35 +0200

    Add sort,filter to Own proposal events

--- src/components/ProposalEvent/MyProposalEvents.tsx
+++ src/components/ProposalEvent/MyProposalEvents.tsx
@@ -8,7 +8,7 @@ import { useStore } from "../../contexts/StoreContext";
 import { ProposalEventSortBy } from "../../types/enums/ProposalEventSortBy";
 import { ProposalEventStatus } from "../../types/enums/ProposalEventStatus";
 import { SortOrder } from "../../types/enums/SortOrder";
-import { IProposalEventBasic } from "../../types/ProposalEventBasic";
+import { IProposalEventSearchResource } from "../../types/ProposalEventSearchResource";
 import ProposalEventCard from "../Cards/ProposalEventCard";
 import ProposalEventBasicForm from "../Forms/ProposalEvent/ProposalEventBasicForm";
 
@@ -19,28 +19,52 @@ interface BodyProps{
 const MyProposalEvents: FC = observer(() => {
 
     const store = useStore();
+    const [filterTitle, setFilterTitle] = useState<string>('');
     const [sortBy, setSortBy] = useState<string>(ProposalEventSortBy.createDate);
     const [sortDirection, setSortDirection] = useState<string>(SortOrder.descending);
     const [filterStatus, setFilterStatus] = useState<string>(ProposalEventStatus.active);
     const [createProposalFormShow, setCreateProposalFormShow] = useState(false);
+    const [currentPage, setCurrentPage] = useState(1);
     const navigate = useNavigate();
     const { enqueueSnackbar } = useSnackbar()
 
     const handleSearchClick = () => {
 
-        //alert("search by "+sortBy+" "+sortDirection+" "+filterStatus)
         store.proposalEventStore.getOwnEvents()
+        setCurrentPage(1);
         if(store.proposalEventStore.isError == true){
             enqueueSnackbar("Failed to execute search.", { variant: 'error'})
         }
     }
+
+    function sortCompare( a:IProposalEventSearchResource, b:IProposalEventSearchResource): number {
+
+        if (sortBy == ProposalEventSortBy.createDate){
+            if (sortDirection == SortOrder.ascending){
+                return a.creationDate! < b.creationDate! ? -1 : 1
+            } else if (sortDirection == SortOrder.descending){
+                return a.creationDate! > b.creationDate! ? -1 : 1
+            }
+        } else if (sortBy == ProposalEventSortBy.title){
+            if (sortDirection == SortOrder.ascending){
+                return a.title! < b.title! ? -1 : 1
+            } else if (sortDirection == SortOrder.descending){
+                return a.title! > b.title! ? -1 : 1
+            }
+        }
+        return 0;
+      }
+
+    const pageSize: number = 2;
+    const totalPageCount = Math.ceil((store.proposalEventStore.ownEvents
+        .filter(event => event.title!.includes(filterTitle) && event.status == filterStatus).length) / pageSize);
     
     return (
         <>
         <Container fluid style={{borderBottom: "9px solid #FBF8F0"}}>
             <div className="row" >
                 <div className="col-9">
-                    <input className="form-control" placeholder="Search by name"/>
+                    <input className="form-control" value={filterTitle} onChange={event => { setFilterTitle(event.target.value); setCurrentPage(1)}} placeholder="Search by name"/>
                 </div>
                 <div className="col-3">
                     <Button variant="outline-success" disabled={store.userStore.user == null} className="w-100" onClick={() => handleSearchClick()}>
@@ -54,40 +78,43 @@ const MyProposalEvents: FC = observer(() => {
                         <div className="input-group-prepend">
                             <span className="input-group-text" id="inputGroup-sizing-default">Sort by</span>
                         </div>
-                        <select className="form-select"  
+                        <select defaultValue={sortBy} className="form-select"  
                             onChange={selectedOption => {
                                 if (Array.isArray(selectedOption)) {
                                     throw new Error("Unexpected type passed to ReactSelect onChange handler");
                                 }
                                 setSortBy(selectedOption.target.value);
+                                setCurrentPage(1);
                                 }}
                             aria-label="Default select example">
-                            <option selected value={ProposalEventSortBy.createDate}>Created date</option>
+                            <option value={ProposalEventSortBy.createDate}>Created date</option>
                             <option value={ProposalEventSortBy.title}>Title</option>
                         </select>
-                        <select className="form-select"
+                        <select defaultValue={sortDirection} className="form-select"
                             onChange={selectedOption => {
                                 if (Array.isArray(selectedOption)) {
                                     throw new Error("Unexpected type passed to ReactSelect onChange handler");
                                 }
                                 setSortDirection(selectedOption.target.value);
+                                setCurrentPage(1);
                                 }}
                             aria-label="Default select example">
-                            <option selected value={SortOrder.descending}>Descending</option>
+                            <option value={SortOrder.descending}>Descending</option>
                             <option value={SortOrder.ascending}>Ascending</option>
                         </select>
                     </div>
                 </div>
                 <div className="col-2">
-                    <select className="form-select"
+                    <select defaultValue={filterStatus} className="form-select"
                         onChange={selectedOption => {
                             if (Array.isArray(selectedOption)) {
                                 throw new Error("Unexpected type passed to ReactSelect onChange handler");
                             }
                             setFilterStatus(selectedOption.target.value);
+                            setCurrentPage(1);
                             }}
                         aria-label="Default select example">
-                        <option selected value={ProposalEventStatus.active}>Active</option>
+                        <option value={ProposalEventStatus.active}>Active</option>
                         <option value={ProposalEventStatus.inactive}>Inactive</option>
                         <option value={ProposalEventStatus.done}>Done</option>
                     </select>
@@ -103,13 +130,17 @@ const MyProposalEvents: FC = observer(() => {
         </Container>
         <Container className="mt-3" fluid>
             <div>
-                {store.proposalEventStore.ownEvents.map((event) => (
-                    <ProposalEventCard onClick={() => navigate(event.id!.toString())} item={event} key={event.id!}/>            
+                {store.proposalEventStore.ownEvents
+                    .filter(event => event.title?.toLowerCase()!.includes(filterTitle.toLowerCase()) && event.status == filterStatus)
+                    .sort((a,b) => sortCompare(a,b))
+                    .slice(currentPage * pageSize - pageSize, currentPage * pageSize)
+                    .map((event) => (
+                        <ProposalEventCard onClick={() => navigate(event.id!.toString())} item={event} key={event.id!}/>            
                 ))}
             </div>
         </Container>
         <div className="d-flex justify-content-center" style={{}}>
-                <Pagination hidden={store.proposalEventStore.ownEvents.length > 1 ? false : true} count={10} style={{justifyContent:"center"}}/>
+                <Pagination hidden={store.proposalEventStore.ownEvents.length > 1 ? false : true}  count={totalPageCount} page={currentPage} onChange={(e, value) => setCurrentPage(value)} style={{justifyContent:"center"}}/>
         </div>
         <ProposalEventBasicForm
             isCreate={true}

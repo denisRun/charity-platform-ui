878fab6ed51c08bbabce2544c0b7b0789b62bcba
denisRun <polozov.denis77@gmail.com>
2023-03-26 00:36:57 +0200

    default pageSize 5

--- src/types/ProposaSearchRequest.tsx
+++ src/types/ProposaSearchRequest.tsx
@@ -2,7 +2,7 @@ import { ITagResource } from "./TagResource";
 
 export class IProposalSearchRequest{
     pageNumber?: number = 1;
-    pageSize?: number = 1;
+    pageSize?: number = 5;
     name?: string;
     sortField?: string;
     order?: string;

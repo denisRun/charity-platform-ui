4bcec9be7ed900c305f29496f6d86cc4883f6dcc
denisRun <polozov.denis77@gmail.com>
2023-03-26 00:37:11 +0200

    Create Request enums

---
+++ src/types/enums/ProposalOwnerRequestStatusEnum.tsx
@@ -0,0 +1,29 @@
+export class ProposalOwnerRequestStatusEnum {
+    static readonly notStarted = "not_started";
+    static readonly inProgress = "in_progress";
+    static readonly completed = "completed";
+    static readonly aborted = "aborted";
+
+    static toContentString(status?:string): string {
+        let result: string = "None";
+        switch(status) { 
+            case this.notStarted: { 
+               result = "Not Started";
+               break; 
+            } 
+            case this.inProgress: { 
+                result = "In Progress";
+                break; 
+            }
+            case this.completed: { 
+                result = "Completed";
+                break; 
+            }
+            case this.aborted: { 
+                result = "Aborted";
+                break; 
+            }
+         } 
+        return result;
+    }
+  }
---
+++ src/types/enums/ProposalRequestStatusEnum.tsx
@@ -0,0 +1,39 @@
+export class ProposalRequestStatusEnum {
+    static readonly waiting = "waiting";
+    static readonly accepted = "accepted";
+    static readonly inProcess = "in_process";
+    static readonly completed = "completed";
+    static readonly aborted = "aborted";
+    static readonly canceled = "canceled";
+
+    static toContentString(status?:string): string {
+        let result: string = "None";
+        switch(status) { 
+            case this.waiting: { 
+               result = "Waiting";
+               break; 
+            } 
+            case this.accepted: { 
+                result = "Accepted";
+                break; 
+             } 
+            case this.inProcess: { 
+                result = "In Progress";
+                break; 
+            }
+            case this.completed: { 
+                result = "Completed";
+                break; 
+            }
+            case this.aborted: { 
+                result = "Aborted";
+                break; 
+            }
+            case this.canceled: { 
+                result = "Canceled";
+                break; 
+            }    
+         } 
+        return result;
+    }
+  }

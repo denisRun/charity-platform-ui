586e0585f96fd090907e955130f582c9d0428b67
denisRun <polozov.denis77@gmail.com>
2023-03-26 00:35:11 +0200

    update proposal request status form

---
+++ src/components/Forms/ProposalEvent/ProposalEventUpdateRequestForm.tsx
@@ -0,0 +1,103 @@
+import { FC, useState } from "react";
+import { Col, Container, Form, Row } from "react-bootstrap";
+import Button from 'react-bootstrap/Button';
+import { useStore } from '../../../contexts/StoreContext';
+import Modal from 'react-bootstrap/Modal';
+import logo from '../../../images/logo.svg';
+import { ErrorMessage, Formik, FormikHelpers } from "formik";
+import TextLarge from "../../Text/TextLarge";
+import TextFormHeader from "../../Text/TextFormHeader";
+import TextForm from "../../Text/TextForm";
+import { IUserSignupRequest } from "../../../types/UserSignupRequest";
+import UserSignupValidation from "../../../validations/UserSignupValidation";
+import { useSnackbar } from "notistack";
+import { ProposalRequestCreateRequest } from "../../../types/ProposalRequestCreateRequest";
+
+interface IRequestFormProps{
+    show: boolean;
+    onHide: () => void;
+}
+
+const ProposalEventUpdateRequestForm: FC<IRequestFormProps> = (props) => {
+
+    const store = useStore();
+    const initialValues = new ProposalRequestCreateRequest();
+    const { enqueueSnackbar } = useSnackbar()
+    const [val, setVal] = useState("");
+    
+    const updateRequestSubmit = async () => {
+
+        // request.id = store.proposalEventStore.event.id!;
+        // await store.proposalEventStore.addEventRequest(request)
+        if(store.proposalEventStore.isError == false){
+            props.onHide();
+            enqueueSnackbar("Request created.", { variant: 'success'})
+        } else{
+            enqueueSnackbar("Failed to create Request.", { variant: 'error'})
+        }
+      };
+
+    return (
+        <Modal
+            className="modal-xl"
+            show={props.show}
+            onHide={props.onHide}
+            aria-labelledby="contained-modal-title-vcenter"
+            centered>
+            <Modal.Header className="mt-3 ms-4 me-4">
+                <Modal.Title id="contained-modal-title-vleft">
+                    <TextFormHeader> Update request status </TextFormHeader>
+                </Modal.Title>
+                <Modal.Title id="contained-modal-title-vright">
+                    <img src={logo} style={{ width: 110, height: 22 }} />
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form noValidate onSubmit={updateRequestSubmit}>
+                    <Container fluid>
+                        <Row className="ms-3 me-3 mb-2">
+                            <TextForm> Description: </TextForm>
+                        </Row>
+                        <Row className="ms-3 me-3 ps-0">
+                            <Form.Control
+                                    as="textarea" 
+                                    rows={3}
+                                    type="textarea"
+                                    name="comment"
+                                    placeholder="Your Request details"
+                                    value={val}
+                                    onChange={x => setVal(x.target.value)}
+                            />
+                        </Row>
+                        {val.length! > 0 ? 
+                        <>
+                            <Row className="ms-3 mt-3 me-3 mb-2">
+                                <TextForm> Description: </TextForm>
+                            </Row>
+                            <Row className="ms-3 me-3 ps-0">
+                                <Form.Control
+                                        as="textarea" 
+                                        rows={3}
+                                        type="textarea"
+                                        name="comment"
+                                        placeholder="Your Request details"
+                                        value={val}
+                                        onChange={x => setVal(x.target.value)}
+                                />
+                            </Row>
+                        </> 
+                        :
+                         <></>}
+                        <Row className="justify-content-md-center ms-3 me-3 mt-4 mb-3">
+                                <Button style={{fontSize:"1.3rem"}} variant="success" type="submit">
+                                    Submit
+                                </Button>
+                        </Row>
+                    </Container>
+                </Form>
+            </Modal.Body>
+        </Modal>
+    );
+};
+
+export default ProposalEventUpdateRequestForm;

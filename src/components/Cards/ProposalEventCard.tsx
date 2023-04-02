83d335c7142a8d0af90da8c15eaccebd652c820a
denisRun <polozov.denis77@gmail.com>
2023-04-02 23:51:36 +0300

    Add ownEvent settings

--- src/components/Cards/ProposalEventCard.tsx
+++ src/components/Cards/ProposalEventCard.tsx
@@ -1,15 +1,31 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { IProposalEventSearchResource } from "../../types/ProposalEventSearchResource";
 import dateTimeFormatter from "../../Helpers/ToDateTimeConverter";
+import { useLocation } from 'react-router-dom';
+import { useSnackbar } from "notistack";
+import ComplainForm from "../Forms/ComplainForm";
+import { ProposalEventStatusEnum } from "../../types/enums/ProposalEventStatusEnum";
+import { EventTypeEnum } from "../../types/enums/EventTypeEnum";
 
 interface ProposalEventCardProps{
     onClick: () => void;
+    isOwn: boolean;
     item: IProposalEventSearchResource;
     key: number;
 }
 
 const ProposalEventCard: FC<ProposalEventCardProps> = (props) => {
+
+    const location = useLocation();
+    const { enqueueSnackbar } = useSnackbar();
+    const [complainFormShow, setComplainFormShow] = useState(false);
+
+
+    const handleCopyClick = async () => {
+        navigator.clipboard.writeText(`http://localhost:3000${location.pathname}/${props.item.id}`);
+        enqueueSnackbar("Link copied.", { variant: 'success'})
+    }
 
     return (
         <div className="card w-100 mb-3 card-hover" style={{height:160}}>
@@ -30,15 +46,28 @@ const ProposalEventCard: FC<ProposalEventCardProps> = (props) => {
                     </div>
                 </div>
                 <div className="col-2-5 mt-3" >
-                    <div className="mt-1 mt-2 ms-2" >
+                    <div className="mt-1 mt-2 ms-2" hidden={props.isOwn}>
                         <h6>
                             {props.item.authorInfo?.username}
                             <img src={props.item.authorInfo?.profileImageURL} className="rounded-circle ms-2" style={{width:40, height:40}} alt="Avatar" />
                         </h6>
                     </div>
-                    <div className="mt-3 ms-2" >
+                    <div className="mt-3 ms-2" hidden={props.isOwn}>
                         <h6>
                             {props.item.availableHelps! > 0 ? <>Available <span className="dot-green ms-2"> </span></> : <>Work in progress <span className="dot-orange ms-2"> </span></> }
+                        </h6>
+                    </div>
+                    <div className="mt-1 mt-2 ms-2" hidden={!props.isOwn}>
+                        <h6>
+                            Status: 
+                            <span className="fw-bold ms-1">
+                                 {ProposalEventStatusEnum.toContentString(props.item.status)} 
+                            </span>
+                        </h6>
+                    </div>
+                    <div className="mt-4 ms-2" hidden={!props.isOwn}>
+                        <h6>
+                            Active requests: <span className="fw-bold">{(props.item.maxConcurrentRequests! - props.item.availableHelps!)}</span> / {props.item.maxConcurrentRequests}
                         </h6>
                     </div>
                     <div className="mt-4 ms-2" >
@@ -49,12 +78,17 @@ const ProposalEventCard: FC<ProposalEventCardProps> = (props) => {
                     <div className="btn-group">
                         <button type="button" id="userProfileActions" className="btn fs-5" data-bs-toggle="dropdown" data-bs-display="static" aria-expanded="false"><MoreVertIcon fontSize='large' /></button>
                         <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="userProfileActions">
-                        <li><button className="dropdown-item" type="button" onClick={() => console.log()}>Copy link</button></li>
-                        <li><button className="dropdown-item" type="button" onClick={() => console.log()}>Complain</button></li>
+                        <li><button className="dropdown-item" type="button" onClick={() => handleCopyClick()}>Copy link</button></li>
+                        <li><button className="dropdown-item" type="button" onClick={() => setComplainFormShow(true)}>Complain</button></li>
                         </ul>
                     </div>
                 </div>
             </div>
+            <ComplainForm 
+                eventId={props.item.id!}
+                eventType={EventTypeEnum.proposal}
+                show={complainFormShow}
+                onHide={() => setComplainFormShow(false)} />
         </div>
   )};
 

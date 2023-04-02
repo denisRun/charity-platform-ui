63196334d631edc3f774c13d1a06e023c9de336b
denisRun <polozov.denis77@gmail.com>
2023-04-03 01:20:49 +0300

    update notification styles

--- src/components/Cards/NotificationCard.tsx
+++ src/components/Cards/NotificationCard.tsx
@@ -14,23 +14,21 @@ interface NotificationCardProps{
 const NotificationCard: FC<NotificationCardProps> = (props) => {
 
     return (
-            <div className="row ms-3 me-3 mb-3 border rounded card-hover" onClick={() => props.onClick()}>
-                <div className="col-1 pt-2 d-flex align-items-center">
+            <div className="row ms-3 me-3 mb-4 border rounded card-hover" onClick={() => props.onClick()}>
+                <div className="col-1 pt-1 d-flex align-items-center">
                     { !props.item.isRead ? <span className="dot-orange ms-2"> </span> : <span className="dot-grey ms-2"> </span> }
                 </div>
-                <div className="col-11 pt-2">
+                <div className="col-11 pt-3 pb-2">
                     <div className="row">
-                        <h5>
-                            {EventTypeEnum.toContentString(props.item.eventType) + " '" + props.item.eventTitle + "' request status updated to " + ProposalRequestStatusEnum.toContentString(props.item.newStatus)}
+                        <h5 className="mb-0 pb-0">
+                            {EventTypeEnum.toContentString(props.item.eventType)} <span className="fw-bold">{props.item.eventTitle}</span>, {"request status updated to " + ProposalRequestStatusEnum.toContentString(props.item.newStatus)}
                         </h5>
-                    </div>  
-                    <div className="row">
                         <h6 className="fw-bold">
-                            <span className="" style={{fontSize: 12, color:"gray"}}>
+                            <span style={{fontSize: 12, color:"gray"}}>
                                 {dateTimeFormatter(props.item.createdAt!)}
                             </span>
                         </h6>
-                    </div>
+                    </div>  
                 </div>
             </div>
   )};

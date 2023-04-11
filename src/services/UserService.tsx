3a57f8a67e10b6803613b11af76e2ed40cac430c
denisRun <polozov.denis77@gmail.com>
2023-04-12 02:17:38 +0300

    remove unused

--- src/services/UserService.tsx
+++ src/services/UserService.tsx
@@ -11,10 +11,6 @@ export interface IUserService{
     refreshUserData(refreshToken: string): Promise<IUserResource>;
     signup(credentials: IUserSignupRequest): Promise<IUserResource>;
     changePassword(): Promise<void>;
-    // getUsers(query: string): Promise<IUser[]>;
-    // getUser(id: string): Promise<IUser>;
-    // createUser(user: IUser): Promise<IUser>;
-    // updateUser(id: string, user: IUser): Promise<IUser>;
     getNotifications(): Promise<INotificationResource[]>;
     readNotifications(ids: string[]): Promise<void>;
 }
@@ -58,34 +54,6 @@ class UserService implements IUserService{
         }
         const response = await axios.put<void>(apiControllerPath + "/read-notifications", JSON.stringify(request));
         return;
-    }
-
-    // ALL the following should be REMOVED
-
-
-    async getUsers(query: string): Promise<IUserResource[]> {
-        const request: UserSearchRequest = {
-            query:query,
-            pageNumber:1,
-            pageSize:20
-        };
-        const response = await axios.post<IPagedResultSet<IUserResource>>(authControllerPath + '/search', request);
-        return response.data.items;
-    }
-
-    async getUser(id?: string): Promise<IUserResource> {
-        const response = await axios.get<IUserResource>(authControllerPath + "/" + id);
-        return response.data;
-    }
-
-    async createUser(user: IUserResource): Promise<IUserResource> {
-        const response = await axios.post<IUserResource>(authControllerPath, JSON.stringify(user));
-        return response.data;
-    }
-
-    async updateUser(id: string, user: IUserResource): Promise<IUserResource> {
-        const response = await axios.put<IUserResource>(authControllerPath + "/" +id, JSON.stringify(user));
-        return response.data;
     }
 }
 
